6836a3a4ef1f87a947f8542f6d4e4ec02661dd40
AubreyAmoah <2gee419@gmail.com>
2025-01-25 12:46:17 +0000

    cors fixing continued

--- src/config/cors/middleware/credentials.js
+++ src/config/cors/middleware/credentials.js
@@ -1,11 +1,11 @@
-const allowedOrigins = require("../allowedOrigins.js");
+import allowedOrigins from "../allowedOrigins.js";
 
 const credentials = (req, res, next) => {
-    const origin = req.headers.origin;
-    if ( allowedOrigins.includes(origin)) {
-        res.header('Access-Control-Allow-Credentials', true);
-    }
-    next();
-}
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Credentials", true);
+  }
+  next();
+};
 
-export default credentials
+export default credentials;
