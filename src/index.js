74e1901467a583cb14f77308ad926b94ff5fabd3
AubreyAmoah <2gee419@gmail.com>
2025-01-25 12:41:00 +0000

    cors update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import express from 'express'
-import path from 'path'
+import express from "express";
+import path from "path";
+import cors from "cors";
 import { fileURLToPath } from "url";
 import cookieParser from "cookie-parser";
 
+import credentials from "./config/cors";
+import corsOptions from "./config/cors/corsOptions.js";
+
 import authMiddleware from "./middleware/authMiddleware.js";
-import authRoutes from './api/auth/auth.router.js'
+import authRoutes from "./api/auth/auth.router.js";
 
 const app = express();
 const PORT = process.env.PORT || 5500;
@@ -13,13 +17,15 @@ const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 
+app.use(credentials);
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "../public")));
 
-
 app.use(cookieParser());
-
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/help", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
